f8426c55ae49700bf1054ebb6d736676b669860a
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-10-17 18:47:58 +0800

    Implement MLP for Indoor Localization

--- IndoorLocalization/MultiLayerPerceptronModule.py
+++ IndoorLocalization/MultiLayerPerceptronModule.py
@@ -1,22 +1,26 @@
 from torch import Tensor
-from torch.nn import Module, Sequential
+from torch.nn import Module
 from torch.nn import Flatten, Linear, ReLU
 from torch.nn.init import normal_
 
 
 class MultiLayerPerceptronModule(Module):
 
-    def __init__(self) -> None:
+    def __init__(self, device) -> None:
         super().__init__()
         self.flatten = Flatten()
-        self.linear_relu_stack = Sequential(Linear(7, 6), ReLU(), Linear(6, 4))
-        self.linear_relu_stack.apply(self.init_weights)
+        self.linear1 = Linear(7, 12, device=device)
+        self.linear1.apply(self.init_weights)
+        self.relu = ReLU()
+        self.linear2 = Linear(12, 4, device=device)
+        self.linear2.apply(self.init_weights)
 
     def init_weights(self, module) -> None:
-        if isinstance(module, Linear):
-            normal_(module.weight, std=0.01)
+        normal_(module.weight, std=0.01)
 
     def forward(self, X) -> Tensor:
-        X = self.flatten(X)
-        y = self.linear_relu_stack(X)
-        return y
+        X = self.flatten(X).to(self.linear1.weight.dtype)
+        X = self.linear1(X)
+        X = self.relu(X)
+        X = self.linear2(X)
+        return X
